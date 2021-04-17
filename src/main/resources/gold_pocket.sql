be0ad2214c90ce6d4e84de8902df3c923818313d
Jution Candra Kirana <jution.kirana@enigmacamp.com>
2021-04-17 13:17:45 +0700

    Add product struktur data table

---
+++ src/main/resources/gold_pocket.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE gold_pocket;
+
+DROP TABLE IF EXISTS m_product;
+CREATE TABLE m_product (
+    id int4 NOT NULL,
+    product_name varchar(100) COLLATE "pg_catalog"."default",
+    price numeric
+);
+
+-- ----------------------------
+-- Primary Key structure for table m_product
+-- ----------------------------
+ALTER TABLE m_product ADD CONSTRAINT product_pkey PRIMARY KEY (id);
